777b7a02af85e1c1464b0e45345d4276400799ef
Tina Liu <tina.chang.liu@gmail.com>
2019-05-21 23:10:05 +0900

    added seed and populated database with seed

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'faker'
+
 # FRONT END GEMS ----->
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'creating restaurants and reviews...'
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{%w(London York Bristol).sample}",
+    category: Restaurant::CATEGORY.sample,
+    phone_number: "020 #{rand(1001..9000)} #{rand(1001..9000)}"
+  )
+  rand(1..5).times do
+    review = Review.new(content: Faker::Restaurant.review, rating: rand(1..5))
+    review.restaurant = restaurant
+    review.save!
+  end
+  # restaurant.save!
+end
+
+puts 'finished'
