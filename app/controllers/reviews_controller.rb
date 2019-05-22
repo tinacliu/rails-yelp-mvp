1553da2350bc1ece9ee48d631f3491d51fe22a63
Tina Liu <tina.chang.liu@gmail.com>
2019-05-22 14:26:51 +0900

    refactored code to have new review form on restaurant show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,9 @@
 class ReviewsController < ApplicationController
   before_action :set_restaurant
 
-  def new
-    @review = Review.new
-  end
+  # def new
+  #   @review = Review.new
+  # end
 
   def create
     @review = Review.new(review_params)
